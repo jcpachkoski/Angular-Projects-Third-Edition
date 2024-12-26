391ae99220e3088f9f4ce939d46d3d4ec775f735
John Pachkoski <jpachkoski@gmail.com>
2024-12-26 04:38:59 -0500

    Modified scully.my-blog.config.ts

--- Chapter02/scully.my-blog.config.ts
+++ Chapter02/scully.my-blog.config.ts
@@ -3,7 +3,9 @@ import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "my-blog",
+  distFolder: "./dist/my-blog/browser",
   outDir: './dist/static',
+  defaultPostRenderers: [],
   routes: {
     '/posts/:id': {
       type: 'contentFolder',
