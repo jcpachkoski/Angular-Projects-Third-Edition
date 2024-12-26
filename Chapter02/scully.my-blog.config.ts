5d167154ee7b2da5dc4de66e7af4bf34983ab85b
John Pachkoski <jpachkoski@gmail.com>
2024-12-26 04:43:31 -0500

    Modified scully config file

--- Chapter02/scully.my-blog.config.ts
+++ Chapter02/scully.my-blog.config.ts
@@ -1,10 +1,11 @@
+// Be sure this file is above the src directory in the root folder!!!
 import { ScullyConfig } from '@scullyio/scully';
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "my-blog",
-  distFolder: "./dist/my-blog/browser",
-  outDir: './dist/static',
+  distFolder: "./dist/my-blog/browser", // output directory of your Angular build artifacts
+  outDir: './dist/static', // directory for scully build artifacts
   defaultPostRenderers: [],
   routes: {
     '/posts/:id': {
